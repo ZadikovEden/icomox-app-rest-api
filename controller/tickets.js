6ff52df98a9ca5885a7edc8c5ddbceb948a20b7d
eden <eden@shiratech-solutions.com>
2021-01-17 10:25:56 +0200

    added a filter function to tickets

--- controller/tickets.js
+++ controller/tickets.js
@@ -1,5 +1,22 @@
 const axios = require('axios');
 const baseUrl = "https://icomox.poc.icomoxlive.net/rest";
+const severity_level_enum = {
+  "0":"normal_operation",
+  "1":"medium_level_alerts",
+  "2":"high_level_alerts"
+}
+const filterTickets = (tickets) => {
+  let filtered_tickets = {
+    "normal_operation":[],
+    "medium_level_alerts":[],
+    "high_level_alerts":[],
+  };
+  tickets.map(ticket => {
+    let severity = severity_level_enum[ticket.alertLevel];
+    filtered_tickets[severity].push(ticket);
+  })
+  return filtered_tickets;
+}
 
 const getAllTickets = async (req, res, next) => {
     const config = {
@@ -11,7 +28,7 @@ const getAllTickets = async (req, res, next) => {
       };    
       axios(config)
       .then(function (response) {
-        res.json(response.data);
+       res.json(filterTickets(response.data));
       })
       .catch(function (error) {
         console.log(error);
